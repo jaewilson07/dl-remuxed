122f21aabfaa6bf1fe98c094fa24ac365165a788
jaewilson07 <jaewilson07@gmail.com>
2025-10-20 21:05:52 +0000

    working on publish

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,24 @@
 __version__ = "0.0.1-alpha"
 
 # Import submodules to make them available
-from . import classes
-from . import client
-from . import routes
+# Note: classes, client, and routes have circular dependencies
+# If you encounter import errors, import individual modules directly like:
+# from domolibrary2.client.auth import DomoAuth
+# from domolibrary2.routes.user import get_user
+# from domolibrary2.classes.DomoUser import DomoUser
+
 from . import utils
 from . import integrations
+from . import client
+from . import routes  
+from . import classes
 
 # Define what gets imported with "from domolibrary2 import *"
 __all__ = [
     "__version__",
     "classes",
     "client",
-    "routes",
+    "routes", 
     "utils",
     "integrations",
 ]
